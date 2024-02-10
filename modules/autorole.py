1b3004eae24d15c7beb761132f83c0858bef566b
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-02-10 21:48:04 +0300

    recent changes

--- modules/autorole.py
+++ modules/autorole.py
@@ -1,3 +1,4 @@
+from nextcord import Embed
 from nextcord.ext.commands import Cog
 
 
@@ -15,9 +16,14 @@ class Autorole(Cog):
         try:
             role = member.guild.get_role(1205479230322778134)
             await member.add_roles(role)
+
+            embed = Embed(title="Welcome",description=f"Welcome to the server {member.mention}",colour=0x00ff00)
+            channel = member.guild.get_channel(1205483966094647376)
+            await channel.send(embed=embed)
             print(f"Gave {member} the role {role}")
         except Exception as e:
             print(f"An exception occured {e}")
+
 
 
 def setup(bot):
