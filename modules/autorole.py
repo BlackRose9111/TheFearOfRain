e1d3c0bc29313366d1b99e0df0b801230f417872
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-02-13 14:10:58 +0300

    recent changes

--- modules/autorole.py
+++ modules/autorole.py
@@ -1,6 +1,8 @@
 from nextcord import Embed
 from nextcord.ext.commands import Cog
 
+from data.objects import RpCharacter
+
 
 class Autorole(Cog):
 
@@ -23,7 +25,25 @@ class Autorole(Cog):
         except Exception as e:
             print(f"An exception occured {e}")
 
+    @Cog.listener()
+    async def on_member_remove(self, member):
+        print("Member left")
+        try:
+            channel = member.guild.get_channel(1205483966094647376)
+            vembed = Embed(title="Goodbye",description=f"{member.display_name()} has left the server.",colour=0xff0000)
+            await channel.send(embed=vembed)
+            list_characters = RpCharacter.find_all(discord_id=str(member.id))
+            t = f"{member.mention} has left the server. The following characters have been archived:\n"
+            for character in list_characters:
+                character.approved = False
+                character.update()
+                t += f"{character.name}\n"
+            vembed = Embed(title="Characters Archived",description=t,colour=0x00ff00)
+            await channel.send(embed=vembed)
 
+
+        except Exception as e:
+            print(f"An exception occured {e}")
 
 def setup(bot):
     bot.add_cog(Autorole(bot))
