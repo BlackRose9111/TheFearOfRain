32510e257f4ae5a2f93f1d1e0efac7a4179218bd
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-02-19 13:47:12 +0300

    19/02/24

--- modules/utility.py
+++ modules/utility.py
@@ -16,7 +16,20 @@ class Utility(Cog):
     async def ping(self,ctx):
         await ctx.send("Pong")
 
+    @ping.error
+    async def ping_error(self,ctx,error):
+        await ctx.send(f"Error: {error}")
 
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def echo(self,ctx,*,message):
+        original_message = ctx.message
+        await original_message.delete()
+        await ctx.send(message)
+
+    @echo.error
+    async def echo_error(self,ctx,error):
+        await ctx.send(f"Error: {error}")
 
 def setup(bot):
     bot.add_cog(Utility(bot))
